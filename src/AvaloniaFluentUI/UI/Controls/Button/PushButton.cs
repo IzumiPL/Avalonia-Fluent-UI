c212ffb8e6afa180832582402e1c00aa01cb5027
Izumi <hanserinterval@outlook.com>
2026-05-13 23:11:37 +0800

    添加标准按钮

---
+++ src/AvaloniaFluentUI/UI/Controls/Button/PushButton.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Media;
+
+namespace AvaloniaFluentUI.UI.Controls;
+
+public class PushButton : Avalonia.Controls.Button
+{
+    public static readonly StyledProperty<Geometry> IconDataProperty =
+        AvaloniaProperty.Register<PushButton, Geometry?>(nameof(IconData));
+
+    public Geometry? IconData
+    {
+        get => GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+}
