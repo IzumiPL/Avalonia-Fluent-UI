e56c544641f238872abb7af5007b37474ddd2ff2
Izumi <hanserinterval@outlook.com>
2026-05-13 23:13:52 +0800

    添加平滑滚动视图

---
+++ src/AvaloniaFluentUI/UI/Controls/SmoothScrollViewer/SmoothScrollViewer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaFluentUI.UI.Controls;
+
+public class SmoothScrollViewer : Avalonia.Controls.ScrollViewer
+{
+    // public SmoothScrollContentPresenter? Presenter { get; private set; }
+    
+    // protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    // {
+        // base.OnApplyTemplate(e);
+
+        // Presenter = e.NameScope.Find("PART_ContentPresenter") as SmoothScrollContentPresenter;
+    // }
+}
