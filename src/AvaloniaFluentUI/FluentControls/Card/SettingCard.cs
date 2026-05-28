ba824467969dbb29fd2109c9ce87bfbf4ddb8812
Izumi <hanserinterval@outlook.com>
2026-05-28 22:55:07 +0800

    添加了 SettingCard 控件

---
+++ src/AvaloniaFluentUI/FluentControls/Card/SettingCard.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace AvaloniaFluentUI.Controls;
+
+public class SettingCard : HeaderedContentControl
+{
+    public static readonly StyledProperty<string> DescriptionProperty =
+        AvaloniaProperty.Register<SettingCard, string?>(nameof(Description));
+
+    public static readonly StyledProperty<Geometry?> IconDataProperty =
+        AvaloniaProperty.Register<SettingCard, Geometry?>(nameof(IconData));
+
+    public Geometry? IconData
+    {
+        get => GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+    
+    public string? Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+    
+    public SettingCard()
+    {
+    }
+}
