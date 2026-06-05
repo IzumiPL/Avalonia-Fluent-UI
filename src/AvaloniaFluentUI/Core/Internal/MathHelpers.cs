349632472a97138a55a709902cb87cdd8144ac1c
Izumi <hanserinterval@outlook.com>
2026-06-05 23:39:10 +0800

    Update MathHelpers

--- src/AvaloniaFluentUI/Core/Internal/MathHelpers.cs
+++ src/AvaloniaFluentUI/Core/Internal/MathHelpers.cs
@@ -41,6 +41,7 @@ public static class MathHelpers
     /// <param name="max">The upper bound of the result.</param>
     /// <returns><see cref="double"/></returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="min"/> is greatest of <paramref name="max"/>.</exception>
+    [Obsolete("Use double clamp methods instead")]
     public static double Clamp(double value, double min, double max)
     {
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
@@ -67,6 +68,7 @@ public static class MathHelpers
     /// <param name="max">The upper bound of the result.</param>
     /// <returns><see cref="int"/></returns>
     /// <exception cref="ArgumentException">Thrown when <paramref name="min"/> is greatest of <paramref name="max"/>.</exception>
+    [Obsolete("Use int.Clamp methods instead")]
     public static int Clamp(int value, int min, int max)
     {
 #if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
@@ -85,58 +87,10 @@ public static class MathHelpers
 #endif
     }
 
-    /// <summary>
-    /// Returns <paramref name="value"/> clamped to the inclusive range of min and max.
-    /// </summary>
-    /// <param name="value">The value to be clamped.</param>
-    /// <param name="min">The lower bound of the result.</param>
-    /// <param name="max">The upper bound of the result.</param>
-    /// <returns><see cref="byte"/></returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="min"/> is greatest of <paramref name="max"/>.</exception>
-    public static byte Clamp(byte value, byte min, byte max)
-    {
-#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
-        return Math.Clamp(value, min, max);
-#else
-        if (min > max)
-            throw new ArgumentException("min is greater than max");
+    public static bool IsZero(double value, double eps = 1e-5) =>
+        double.Abs(value) < eps;
 
-        if (value < min)
-            return min;
-
-        if (value > max)
-            return max;
-
-        return value;
-#endif
-    }
-
-    /// <summary>
-    /// Returns <paramref name="value"/> clamped to the inclusive range of min and max.
-    /// </summary>
-    /// <param name="value">The value to be clamped.</param>
-    /// <param name="min">The lower bound of the result.</param>
-    /// <param name="max">The upper bound of the result.</param>
-    /// <returns><see cref="long"/></returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="min"/> is greatest of <paramref name="max"/>.</exception>
-    public static long Clamp(long value, long min, long max)
-    {
-#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
-        return Math.Clamp(value, min, max);
-#else
-        if (min > max)
-            throw new ArgumentException("min is greater than max");
-
-        if (value < min)
-            return min;
-
-        if (value > max)
-            return max;
-
-        return value;
-#endif
-    }
-
-
+    public static bool IsClose(double value1, double value2, double eps = 1e-5) =>
+        double.Abs(value1 - value2) < eps;
 
 }
