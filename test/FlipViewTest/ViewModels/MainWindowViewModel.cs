2057925fc499e2be5989b9c6d9727a548554757b
Izumi <hanserinterval@outlook.com>
2026-06-05 23:57:10 +0800

    Init FlipViewTest

---
+++ test/FlipViewTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FlipViewTest.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public List<string> Images { get; }
+
+    public MainWindowViewModel()
+    {
+        Images = new List<string>();
+        
+        foreach (var file in Directory.GetFiles(@"C:\Users\Administrator\OneDrive\Pictures\Background"))
+        {
+            if (file.Split(".")[^1] == "png")
+            {
+                Images.Add(file);
+            }
+        }
+    }
+}
---
+++ test/FlipViewTest/Views/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using AvaloniaFluentUI.Windowing;
+
+namespace FlipViewTest.Views;
+
+public partial class MainWindow : AppWindow 
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        
+    }
+}
