eac8f00082966d03e47269620363c5ab3d61f0f7
Izumi <hanserinterval@outlook.com>
2026-06-05 23:51:38 +0800

    Update NumberBox

--- src/AvaloniaFluentUI/FluentControls/NumberBox/NumberBox.cs
+++ src/AvaloniaFluentUI/FluentControls/NumberBox/NumberBox.cs
@@ -505,7 +505,7 @@ public partial class NumberBox : TemplatedControl
         }
     }
 
-    protected override void OnGotFocus(GotFocusEventArgs e)
+    protected override void OnGotFocus(FocusChangedEventArgs e)
     {
         base.OnGotFocus(e);
 
@@ -523,7 +523,7 @@ public partial class NumberBox : TemplatedControl
         }
     }
 
-    protected override void OnLostFocus(RoutedEventArgs e)
+    protected override void OnLostFocus(FocusChangedEventArgs e)
     {
         base.OnLostFocus(e);
 
