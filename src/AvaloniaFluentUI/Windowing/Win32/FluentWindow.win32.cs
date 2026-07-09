5f01a88b7af2b1a1bc9ce95cee5078e2d10375b8
Izumi <hanserinterval@outlook.com>
2026-07-09 15:46:07 +0800

    .

--- src/AvaloniaFluentUI/Windowing/Win32/FluentWindow.win32.cs
+++ src/AvaloniaFluentUI/Windowing/Win32/FluentWindow.win32.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using AvaloniaFluentUI.Controls.Interop;
 
@@ -9,7 +9,9 @@ public partial class FluentWindow
     [MethodImpl(MethodImplOptions.NoInlining)]
     private void InitializeWindowPlatform()
     {
-        Console.WriteLine("Is Window Platform");
+#if DEBUG
+        Debug.WriteLine("Is Window Platform");
+#endif
         
         IsWindows = true;
         IsWindows11 = OSVersionHelper.IsWindows11();
@@ -17,7 +19,7 @@ public partial class FluentWindow
         _win32Manager = new Win32WindowManager(this);
 
         Win32Interop.ApplyTheme(_win32Manager.Hwnd, true);
-        PlatformFeatures = new Win32AppWindowFeatures(this);
+        PlatformFeatures = new Win32FluentWindowFeatures(this);
     }
 
     private Win32WindowManager _win32Manager;
