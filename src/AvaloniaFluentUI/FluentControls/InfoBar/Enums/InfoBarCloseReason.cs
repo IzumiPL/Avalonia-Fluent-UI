78db88e0b280d5b092ec270bebe64d3ae7025332
Izumi <hanserinterval@outlook.com>
2026-07-11 18:11:10 +0800

    移动文件位置

---
+++ src/AvaloniaFluentUI/FluentControls/InfoBar/Enums/InfoBarCloseReason.cs
@@ -0,0 +1,17 @@
+﻿namespace AvaloniaFluentUI.Controls;
+
+/// <summary>
+/// Defines constants that indicate the cause of the InfoBar closure.
+/// </summary>
+public enum InfoBarCloseReason
+{
+    /// <summary>
+    /// The InfoBar was closed by the user clicking the close button.
+    /// </summary>
+    CloseButton = 0,
+
+    /// <summary>
+    /// The InfoBar was programmatically closed.
+    /// </summary>
+    Programmatic = 1
+}
