c20ec616e20238134109c74534e0a201e2712a0d
Izumi <hanserinterval@outlook.com>
2026-07-13 10:51:57 +0800

    添加 GroupHeaderCard 控件

---
+++ src/AvaloniaFluentUI/FluentControlStyles/Controls/Card/GroupHeaderCard.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using AvaloniaFluentUI.Controls;
+
+namespace AvaloniaFluentUI.Controls;
+
+public class GroupHeaderCard : HeaderedItemsControl 
+{
+}
+
+public class GroupHeaderCardItem : SettingCard 
+{
+}
