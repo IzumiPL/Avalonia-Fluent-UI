49afdd423e142f01ed39adcaec7faaea3fd7e988
Izumi <hanserinterval@outlook.com>
2026-07-20 22:26:32 +0800

    更新 Gallery

---
+++ samples/Gallery/ViewModels/Pages/ViewPage/SettingCardPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿using AvaloniaFluentUI.Locale;
+
+namespace Gallery.ViewModels;
+
+public partial class SettingCardPageViewModel : ViewModelBase
+{
+    public override string Title => LocalizationService.Instance.GetString("SettingCard");
+}
