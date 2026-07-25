c8486e4723130badb718f63296b093aefcb0b000
Izumi <hanserinterval@outlook.com>
2026-07-25 21:20:01 +0800

    添加自增转换器,ImageLabel代理

---
+++ src/AvaloniaFluentUI/Converters/AddConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace AvaloniaFluentUI.Converters;
+
+public class AddConverter : IValueConverter 
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double num)
+        {
+            if (parameter != null && double.TryParse(parameter.ToString(), out var p))
+            {
+                num += p;
+            }
+
+            return num;
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/AvaloniaFluentUI/FluentControls/Label/IImageLabelDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia;
+using Avalonia.Media;
+
+namespace AvaloniaFluentUI.Controls;
+
+public interface IImageLabelDelegate
+{
+    void Render(DrawingContext context, Rect rect, CornerRadius radius);
+}
