d6a8ed79a7e7488c0628a2fd7ed77c0d392d2feb
Izumi <hanserinterval@outlook.com>
2026-08-02 12:59:50 +0800

    添加 TimeLine 控件

---
+++ src/AvaloniaFluentUI/FluentControls/TimeLine/TimeLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace AvaloniaFluentUI.Controls;
+
+public class TimeLine : ItemsControl 
+{
+    protected override Type StyleKeyOverride => typeof(ItemsControl);
+
+    protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey)
+    {
+        return new TimeLineItem();
+    }
+    
+    protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey)
+    {
+        recycleKey = null;
+        return item is not TimeLineItem;
+    }
+}
