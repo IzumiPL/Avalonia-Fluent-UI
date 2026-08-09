27faad0aaf85bb4b4e08dc43f651e84e612f0705
Izumi <hanserinterval@outlook.com>
2026-08-09 13:47:46 +0800

    添加 SegmentedToggleItem, 修改Item布局, 新增 SelectionIndicatorBrush,SelectionIndicatorBackground,SelectionIndicatorWidthIsFixed属性

--- src/AvaloniaFluentUI/FluentControls/Segmented/SegmentedToggleView.cs
+++ src/AvaloniaFluentUI/FluentControls/Segmented/SegmentedToggleView.cs
@@ -3,6 +3,8 @@ using System.Threading;
 using Avalonia;
 using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 using Avalonia.Layout;
 using Avalonia.Media;
 using Avalonia.Styling;
@@ -23,19 +25,32 @@ public class SegmentedToggleView : SegmentedView
         set => SetValue(OrientationProperty, value);
     }
 
+    protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey) 
+        => NeedsContainer<SegmentedToggleItem>(item, out recycleKey);
+
+    protected override Control CreateContainerForItemOverride(object? item, int index, object? ecycleKey)
+        => new SegmentedToggleItem();
+
     protected override void PrepareContainerForItemOverride(Control container, object? item, int index)
     {
         base.PrepareContainerForItemOverride(container, item, index);
-        if (container is SegmentedItem segmentedItem)
+        if (container is SegmentedToggleItem segItem && segItem.Content == null)
         {
-            segmentedItem.Classes.Add("Toggle");
-            if (segmentedItem.Content == null)
-            {
-                segmentedItem.Content = item;
-            }
+            segItem.Content = item;
         }
     }
 
+    protected override void SyncContainerSelection(object? selectedItem)
+    {
+        foreach (var item in Items)
+        {
+            if (ContainerFromItem(item) is SegmentedToggleItem container)
+            {
+                container.IsSelected = selectedItem != null && Equals(item, selectedItem);
+            }
+        }
+    }
+    
     protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
     {
         base.OnPropertyChanged(change);
@@ -136,6 +151,47 @@ public class SegmentedToggleView : SegmentedView
             RunSliderAnimation(transform.Value.Transform(new Point(0, 0)));
             _selectedIndicator.Width = width;
             _selectedIndicator.Height = height;
+        }
+    }
+}
+
+public class SegmentedToggleItem : ContentControl
+{
+    public static readonly StyledProperty<bool> IsSelectedProperty =
+        SelectingItemsControl.IsSelectedProperty.AddOwner<SegmentedToggleItem>();
+
+    /// <summary>
+    /// 设置或获取当前项是否选中
+    /// </summary>
+    public bool IsSelected
+    {
+        get => GetValue(IsSelectedProperty);
+        set => SetValue(IsSelectedProperty, value);
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == IsSelectedProperty)
+        {
+            PseudoClasses.Set(":selected", change.GetNewValue<bool>());
+        }
+    }
+
+    protected override void OnPointerReleased(PointerReleasedEventArgs e)
+    {
+        base.OnPointerReleased(e);
+        var pointe = e.GetPosition(this);
+        if (!e.Handled && IsEffectivelyEnabled && new Rect(Bounds.Size).Contains(pointe))
+        {
+            if (Parent is SegmentedToggleView segmented)
+            {
+                var dataItem = segmented.ItemFromContainer(this);
+                if (dataItem != null)
+                {
+                    segmented.SelectedItem = dataItem;
+                }
+            }
         }
     }
 }
